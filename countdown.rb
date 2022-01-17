27492d472ac62b0a9365416f70c2f64a8c1f5d0b
Nicholas <nikhsd899@outlook.com>
2022-01-17 14:49:09 -0800

    commit tree 12978404092140d5f24fb383ba51318d1fa343de

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,3 @@
-#write your code here
-
 def countdown(seconds_to_midnight)
     while seconds_to_midnight > 0
         puts "#{seconds_to_midnight} SECOND(S)!"
@@ -16,4 +14,8 @@ def countdown_with_sleep(seconds_to_midnight)
     end
     "HAPPY NEW YEAR!"
 end
+
+  
+    
+
 
